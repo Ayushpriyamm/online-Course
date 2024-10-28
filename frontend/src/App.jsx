8b2ff41a4d66d2b23aee057bdae0b5495e975a52
Ayush Priyam <ayushpriyam24@gmail.com>
2024-10-28 19:12:34 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-9xl font-bold text-green-800">kaam shuru de BKL</h1>
+      <h1 className="text-4xl font-bold text-red-800">
+        tujhe bhumika didi ki ksm 💖💖💖
+      </h1>
+    </div>
+  );
+}
