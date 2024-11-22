4d8ebc4b079dcef0a7fa6f38eb53604a07e08d55
Ayush Priyam <ayushpriyam24@gmail.com>
2024-11-22 10:16:39 +0530

    persist redux state configured

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,9 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from '../user/userSlice'
 
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
     reducer: {
-        user:userReducer
-    }
-})
+        user: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"]
+        }
+    })
+});
+
+export const persistor = persistStore(store);
